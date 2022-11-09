4e3758e3b7a167a1fbdc4e5e3ee91b487cffeb5c
juliancape <73551229+juliancape@users.noreply.github.com>
2022-11-09 12:50:19 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-#Fichero para la creacion del objeto compartido
-
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-exts = (cythonize("cy_orbit.pyx"))
-
-setup(ext_modules = exts)
